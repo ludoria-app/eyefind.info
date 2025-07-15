122a7c5bc3175e81c73056d0d6b08c4a5c0f94c4
JoseMa Esparev <esparev@hotmail.com>
2025-07-14 21:14:17 -0600

    tada: 🎉 begin eyefind.info project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.igdb.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.ludoria.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.ludoria.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/types/index.d.ts
@@ -0,0 +1,7 @@
+interface Children {
+  readonly children?: React.ReactNode | Array<React.ReactNode>;
+}
+interface PersonalizedStyles {
+  readonly className?: string;
+  readonly style?: React.CSSProperties;
+}
