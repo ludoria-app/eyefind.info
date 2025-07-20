c06be87d12708c5550515a465a40f85b029256c6
JoseMa Esparev <esparev@hotmail.com>
2025-07-19 23:01:47 -0600

    feat: ✨ add mock data for bleets and websites to enhance development experience

---
+++ src/mocks/bleets.ts
@@ -0,0 +1,59 @@
+/* eslint-disable quotes */
+export const bleets = [
+  {
+    author: 'sarahboptician',
+    content: 'Could someone please give me directions to my happy place?',
+  },
+  {
+    author: 'slappball2',
+    content: '#Themostridiculouslylonghashtagever',
+  },
+  {
+    author: 'esparev',
+    content: 'Go follow @ludoria.app on every platform!',
+  },
+  {
+    author: 'ludoria',
+    content: 'Hey! Great to see you here! ;)',
+  },
+  {
+    author: 'ludoria',
+    content: "Don't search ludoria on eyefind.info, it's not a real website!",
+  },
+  {
+    author: 'zachdealba',
+    content: "Me, some bubble wrap, a bottle of rum. I'd say I got myself a party.",
+  },
+  {
+    author: 'sophiephair',
+    content: "I'm starting to think spending $30,000 on a liberal arts degree was a mistake",
+  },
+  {
+    author: 'biggavin88',
+    content: 'no way that hot english royal got totally busted for weed in LS!!!',
+  },
+  {
+    author: 'deeptammiboo',
+    content: 'Y duz luv hav 2 bee so compliK8ed?',
+  },
+  {
+    author: 'TraceyDeSanta',
+    content: "Please stop sending me toys. I don't do that stuff on cam.",
+  },
+  {
+    author: 'TraceyDeSanta',
+    content: "@JimmyDS infantile genitalia is real medical condition so you should totally use the handicapped parking space",
+  },
+  {
+    author: 'LesterC',
+    content: "Badger Communications will rue the day they ever dropped my wifi connection.",
+  },
+  {
+    author: 'JimmyDS',
+    content: "2 werds 4 u tracey — CLOWN — POCKET.",
+  },
+  {
+    author: 'DrFriedlander',
+    content: "My patients always come first. Especially during sexual surrogate therapy. www.dr-friedlander.com",
+  },
+];
